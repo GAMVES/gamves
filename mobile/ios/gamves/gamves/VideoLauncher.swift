5142a4a50d12f135f89bc777820579a6687bccb3
Jose Vigil <josemanuelvigil@gmail.com>
2018-01-08 23:49:31 -0300

    audio chat...progress.

--- mobile/ios/gamves/gamves/VideoLauncher.swift
+++ mobile/ios/gamves/gamves/VideoLauncher.swift
@@ -514,7 +514,7 @@ class VideoLauncher: UIView, KeyboardDelegate {
 
     func swipeLeftClose()
     {
-
+        
         if self.videoPlayerView.isVideoDown
         {
 
@@ -543,9 +543,7 @@ class VideoLauncher: UIView, KeyboardDelegate {
                         subview.removeFromSuperview()
                     }
                 }
-                
             })
-
         }
     }  
 
@@ -589,7 +587,6 @@ class VideoLauncher: UIView, KeyboardDelegate {
         
         self.chatView.frame.size.height = self.originaChatHeightPosition        
     }
-
 
     /*func draggedView(sender: UIPanGestureRecognizer)
     {
