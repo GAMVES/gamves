e85134def3cdf4aad11a61cfdee521b52176a17a
Jose Vigil <josemanuelvigil@gmail.com>
2018-02-03 21:51:53 -0300

    Fixed fanpageid on approval.

--- mobile/ios/gamvesparents/gamvesparents/FanpageApprovalLauncher.swift
+++ mobile/ios/gamvesparents/gamvesparents/FanpageApprovalLauncher.swift
@@ -239,7 +239,7 @@ class FanpageApprovalLauncher: UIView {
             let apprY = Int(fanpageApprovalView.frame.height) + Int(infoApprovalView.frame.height)
             let apprFrame = CGRect(x: 0, y: apprY, width: Int(keyWindow.frame.width), height: chatHeight)
             
-            buttonsApprovalView = ButtonsApprovalView(frame: apprFrame, obj: fanpageApprovalView, referenceId: nil, delegate: self.delegate)
+            buttonsApprovalView = ButtonsApprovalView(frame: apprFrame, obj: fanpageApprovalView, referenceId: fanpageId, delegate: self.delegate)
             
             buttonsApprovalView.backgroundColor = UIColor.gamvesBackgoundColor
             view.addSubview(buttonsApprovalView)
