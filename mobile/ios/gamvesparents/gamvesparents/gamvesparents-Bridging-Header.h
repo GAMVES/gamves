50428e8950eda38653196af41d49b831e57f3845
Jose Vigil <josemanuelvigil@gmail.com>
2019-06-02 19:45:11 -0300

    Xcode upgrade 10.2.1

--- mobile/ios/gamvesparents/gamvesparents/gamvesparents-Bridging-Header.h
+++ mobile/ios/gamvesparents/gamvesparents/gamvesparents-Bridging-Header.h
@@ -2,8 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "DownPicker/DownPicker.h";
-#import "BEMCheckBox/BEMCheckBox.h";
+
+#import "DownPicker/DownPicker.h"
+#import "BEMCheckBox/BEMCheckBox.h"
 #import "RSKImageCropper/RSKImageCropViewController.h"
 
-//@property (strong, nonatomic) DownPicker *downPicker;
